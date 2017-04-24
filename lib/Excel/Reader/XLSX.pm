3d87afac742d091c4841f07ef92c2d439e6b0fa6
Emmanuel Dupuis <edupuis@invoke.lu>
2017-04-24 17:16:05 +0200

    v0.001_009

--- lib/Excel/Reader/XLSX.pm
+++ lib/Excel/Reader/XLSX.pm
@@ -27,7 +27,7 @@ use Excel::Reader::XLSX::Package::Styles;
 Archive::Zip::setErrorHandler( sub { die shift } );
 
 our @ISA     = qw(Exporter);
-use version; our $VERSION = version->declare("v0.001_008");
+use version; our $VERSION = version->declare("v0.001_009");
 
 # Error codes for some common errors.
 our $ERROR_none                      = 0;
